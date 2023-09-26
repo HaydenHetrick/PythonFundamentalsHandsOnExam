ead307ac4b1ef19c9232a632745f98bcc31834f8
HaydenHetrick <hetrickhay@gmail.com>
2023-09-26 09:25:04 -0400

    Update python_fundamentals_exam.py

--- python_fundamentals_exam.py
+++ python_fundamentals_exam.py
@@ -1,3 +1,27 @@
+age = 17
+temperature = 59
+too_hot = temperature > 70.0
+too_cold = temperature < 60.00
+
+print(age)
+print(temperature)
+print(too_hot)
+print(too_cold) 
+
+addition = age + temperature
+subtraction = age - temperature
+multiplication = age * temperature
+division = age / temperature
+square = age ** 2
+
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
+print(f"Square: {square}")
 
 intro = "Hello World"
 print("intro")
+
+first_name = input("Please Enter Your First name: ")
+print(first_name)
