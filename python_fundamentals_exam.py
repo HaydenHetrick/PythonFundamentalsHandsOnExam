6b2d8583a9562b07e0f04bb4c82178467b916f33
HaydenHetrick <hetrickhay@gmail.com>
2023-09-26 09:43:04 -0400

    Task 4 Complete

--- python_fundamentals_exam.py
+++ python_fundamentals_exam.py
@@ -1,3 +1,5 @@
+# Task 1: Data Types
+
 age = 17
 temperature = 59
 too_hot = temperature > 70.0
@@ -25,3 +27,31 @@ print("intro")
 
 first_name = input("Please Enter Your First name: ")
 print(first_name)
+
+# Task 2: Functions
+
+def calculate_area(Length, Width):
+    area = length * width
+    return area
+
+length = 5.0 
+width = 3.0
+area = calculate_area(length, width)
+print(f"The Rectangles Area is: {area}")
+
+# Task 3: Logic Control
+
+def check_number(number):
+    if number > 0:
+        print(f"{number} is positive ")
+    elif number < 0:
+        print(f"{number} is negative ")
+    else:
+        print(f"{number} is zero ")
+
+check_number(5)
+check_number(-2)
+check_number(0)
+
+# Task 4: Loops
+
